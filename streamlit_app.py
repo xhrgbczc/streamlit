47ac405b8e1d844a72e7bb0dd1c108b2680b255c
tramper <791672300@qq.com>
2025-05-28 19:27:24 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,6 +6,19 @@ st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
 
-components.iframe("https://www.baidu.com", height=500)
+# components.iframe("https://www.baidu.com", height=500)
+pages = {
+    "Page": [
+        st.Page("page_1.py", title="Page 1"),
+        st.Page("page_1.py", title="Page 2"),
+    ],
+    "Resources": [
+        st.Page("learn.py", title="Learn about us"),
+        st.Page("trial.py", title="Try it out"),
+    ],
+}
+
+pg = st.navigation(pages)
+pg.run()
 
 
