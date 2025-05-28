99c5c7f3d1d9b1867087a0974b0ec9a992f963ae
tramper <791672300@qq.com>
2025-05-28 19:43:51 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,10 @@
 import streamlit as st
-import streamlit.components.v1 as components
+# import streamlit.components.v1 as components
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+# st.title("🎈 My new app")
+# st.write(
+#    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+# )
 
 # components.iframe("https://www.baidu.com", height=500)
 pages = {
