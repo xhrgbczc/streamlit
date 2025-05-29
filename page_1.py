e56ced3750fda80a885b039abba8c2f809c06a5d
tramper <791672300@qq.com>
2025-05-29 09:13:16 +0800

    Update page_1.py

--- page_1.py
+++ page_1.py
@@ -1,3 +1,11 @@
 import streamlit as st
+import streamlit.components.v1 as components
 
 st.title("Page 1")
+
+st.title("🎈 Google")
+# st.write(
+#    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+# )
+
+components.iframe("https://www.google.com", height=800)
