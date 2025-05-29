1ae68e8587eb4d7d6d9713ae85072e59e8184de8
tramper <791672300@qq.com>
2025-05-29 13:19:27 +0800

    Update page_1.py

--- page_1.py
+++ page_1.py
@@ -8,4 +8,4 @@ st.title("🎈 Google")
 #    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 # )
 
-components.iframe("https://sv.kesug.com/test.php", height=800)
+components.iframe("https://sv.kesug.com/index.php", height=800)
